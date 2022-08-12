ed7a49c6dae13ec2ff47fa2e063b953d7d790e57
rizky ferdian <rizkyferdian002@gmail.com>
2022-08-12 08:19:29 +0700

    Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use App\Http\Controllers\UserController;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +20,21 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
 Route::get('/checkout', function () {
     return view('checkout');
 })->name('checkout');
 
-
-Route::get('/success-checkout', function () {
+Route::get('/succes-checkout', function () {
     return view('success_checkout');
 })->name('success-checkout');
+
+//Socialite 
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('login/google/callback', [UserController::class, 'callback']);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
